f3ac84bf9cc13bc9588dfada0c1d87671d38e664
zhujingle <js01@86sb.net>
2021-06-23 14:08:28 +0800

    TS-高级类型

--- ts_learning/js_file/07_advanceType.js
+++ ts_learning/js_file/07_advanceType.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // 高级类型
 // 1. 交叉类型
 function extend(first, second) {
@@ -46,3 +61,100 @@ function fun001(name, age) { }
 fun001('hello', 12);
 fun001('hello', undefined);
 fun001('hello');
+function getName(n) {
+    if (typeof n == 'string') {
+        return n;
+    }
+    else {
+        return n();
+    }
+}
+var P;
+var s = p.name;
+var UIElement = /** @class */ (function () {
+    function UIElement() {
+    }
+    UIElement.prototype.animate = function (dx, dy, easing) {
+        if (easing == 'ease-in') {
+            // ...
+        }
+        else if (easing == 'ease-out') {
+            // ...
+        }
+    };
+    return UIElement;
+}());
+var button = new UIElement();
+button.animate(0, 0, 'ease-in');
+// button.animate(0,0,'unease'); // error  除 Easing 声明的三个字符串以外 传入其他的值会报错
+// 数字字面量类型
+function rollDie() {
+    // ...
+    return 1;
+}
+function area(s) {
+    switch (s.kind) {
+        case "square": return s.size * s.size;
+        case "rectangle": return s.height * s.width;
+        case "circle": return Math.PI * Math.pow(s.radius, 2);
+    }
+}
+// 完整性检查
+function area2(s) {
+    switch (s.kind) {
+        case "square": return s.size * s.size;
+        case "rectangle": return s.height * s.width;
+        case "circle": return Math.PI * Math.pow(s.radius, 2);
+        default: return assertNever(s);
+    }
+}
+function assertNever(s) {
+    throw new Error("error!");
+}
+// 多态的 this 类型：表示的是某个包含类或接口的子类型；
+var BasicCalculator = /** @class */ (function () {
+    function BasicCalculator(value) {
+        if (value === void 0) { value = 0; }
+        this.value = value;
+    }
+    BasicCalculator.prototype.currentValue = function () {
+        return this.value;
+    };
+    BasicCalculator.prototype.add = function (operand) {
+        this.value += operand;
+        return this;
+    };
+    BasicCalculator.prototype.multiply = function (operand) {
+        this.value *= operand;
+        return this;
+    };
+    return BasicCalculator;
+}());
+var instance = new BasicCalculator(10).add(5).multiply(2).currentValue();
+// 以上这个类使用了 this 类型，可以继承它
+var extendClass = /** @class */ (function (_super) {
+    __extends(extendClass, _super);
+    function extendClass(value) {
+        if (value === void 0) { value = 0; }
+        return _super.call(this, value) || this;
+    }
+    extendClass.prototype.sin = function () {
+        this.value = Math.sin(this.value);
+        return this;
+    };
+    return extendClass;
+}(BasicCalculator));
+var outputVal = new extendClass(2).multiply(5).sin().add(1).currentValue();
+// 索引类型
+// keyof T 表示 T 上已知的公共属性名的联合 例：keyof Person -> 'name' | 'age'
+// T[K] 索引访问操作符 
+function pluck(o, names) {
+    return names.map(function (n) { return o[n]; });
+}
+var P3 = {
+    name: 'hh',
+    age: 18
+};
+var strings = pluck(P3, ['name']);
+var keys; // string
+var val009; // number
--- ts_learning/ts_file/07_advanceType.ts
+++ ts_learning/ts_file/07_advanceType.ts
@@ -46,4 +46,160 @@ fun001('hello',12);
 fun001('hello',undefined);
 fun001('hello');
 
+// 类型别名：类型别名会给一个类型起个新名(只是创建了一个新名来引用那个类型)
+type Name = string;
+type NameResolve = () => string;
+type NameOrResolve = Name | NameResolve;
+function getName(n: NameOrResolve):Name{
+    if(typeof n == 'string'){
+        return n;
+    }else{
+        return n();
+    }
+}
+// 同接口一样类型别名也可以是泛型
+type Container<T> = {value: T};
+// 类型别名 + 交叉类型
+type LinkedList<T> = T & { next: LinkedList<T> };
+interface Person002 {
+    name: string
+}
+var P: LinkedList<Person>;
+var s = p.name;
 
+
+// 接口 VS 类型别名
+/**
+ * 类型别名不能被 extends 和 implements，也不能 继承和实现其他类型
+ * 尽量使用接口代替类型别名，一般接口无法实现的类型就使用 类型别名
+*/
+type Alias = { num: number };
+interface Interface {
+    num: number
+}
+declare function aliased(arg: Alias): Alias;
+declare function interfaced(arg: Interface): Interface;
+
+
+// 字符串字面量类型
+type Easing = "ease-in" | "ease-out" | "ease-in-out";
+class UIElement {
+    animate(dx: number,dy: number,easing: Easing){
+        if(easing == 'ease-in'){
+            // ...
+        }else if(easing == 'ease-out'){
+            // ...
+        }
+    }
+}
+let button = new UIElement();
+button.animate(0,0,'ease-in');
+// button.animate(0,0,'unease'); // error  除 Easing 声明的三个字符串以外 传入其他的值会报错
+
+
+// 数字字面量类型
+function rollDie(): 1 | 2 | 3 | 4 | 5 | 6 {
+    // ...
+    return 1;
+}
+
+// 可辨识联合：通过合并单例类型、联合类型、类型保护、类型别名来创建的
+interface Square {
+    kind: 'square';
+    size: number;
+}
+interface Rectangle {
+    kind: 'rectangle';
+    width: number;
+    height: number
+}
+interface Circle {
+    kind: 'circle';
+    radius: number;
+}
+type Shape = Square | Rectangle | Circle;
+function area(s: Shape) {
+    switch(s.kind){
+        case "square": return s.size * s.size;
+        case "rectangle": return s.height * s.width;
+        case "circle": return Math.PI*s.radius**2
+    }
+}
+
+
+// 完整性检查
+function area2(s: Shape) {
+    switch(s.kind){
+        case "square": return s.size * s.size;
+        case "rectangle": return s.height * s.width;
+        case "circle": return Math.PI*s.radius**2;
+        default: return assertNever(s)
+    }
+}
+function assertNever(s:never):never {
+    throw new Error("error!")
+}
+
+// 多态的 this 类型：表示的是某个包含类或接口的子类型；
+class BasicCalculator {
+    public constructor(protected value: number = 0){}
+    public currentValue():number {
+        return this.value;
+    }
+    public add(operand: number): this{
+        this.value += operand;
+        return this;
+    }
+    public multiply(operand: number): this{
+        this.value *= operand;
+        return this;
+    }
+}
+let instance = new BasicCalculator(10).add(5).multiply(2).currentValue();
+// 以上这个类使用了 this 类型，可以继承它
+class extendClass extends BasicCalculator {
+    constructor(value = 0) {
+        super(value)
+    }
+    public sin(){
+        this.value = Math.sin(this.value);
+        return this;
+    }
+}
+let outputVal = new extendClass(2).multiply(5).sin().add(1).currentValue();
+
+// 索引类型
+// keyof T 表示 T 上已知的公共属性名的联合 例：keyof Person -> 'name' | 'age'
+// T[K] 索引访问操作符 
+function pluck<T,K extends keyof T>(o:T,names: K[]): T[K][] {
+    return names.map(n => o[n]);
+}
+interface Person003 {
+    name: string;
+    age: number
+}
+let P3:Person003 = {
+    name:'hh',
+    age:18
+}
+let strings:(string | number)[] = pluck(P3,['name']);
+
+
+// 索引类型和字符串索引签名
+interface Data004<T>{
+    [value: string]: T
+}
+let keys: keyof Data004<number>; // string
+let val009: Data004<number>['foo']; // number
+
+
+// 映射类型： 从旧类型中创建新类型的一种方式
+type Readonly001<T> = {
+    readonly [P in keyof T]: T[P]
+}
+type Partial001<T> = {
+    [P in keyof T]?: T[P]
+}
+// 使用
+type ReadonlyPerson = Readonly001<Person003>;
+type PersonPartial = Partial001<Person003>;
